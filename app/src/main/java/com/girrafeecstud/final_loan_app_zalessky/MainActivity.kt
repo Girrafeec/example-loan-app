0af1059153d0bb5aa35d4b87e0090c12ed714de3
Залесский Иван Андреевич <ivan_zal@inbox.ru>
2022-06-09 10:39:49 +0300

    Project initialization

---
+++ app/src/main/java/com/girrafeecstud/final_loan_app_zalessky/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.girrafeecstud.final_loan_app_zalessky
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
